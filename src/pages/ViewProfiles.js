8249efb1ad6def3aadaa258f149bd1c7186e0513
stian-git <82411646+stian-git@users.noreply.github.com>
2023-01-16 18:44:17 +0100

    Working on Profiles-view

--- src/pages/ViewProfiles.js
+++ src/pages/ViewProfiles.js
@@ -1,6 +1,10 @@
+// Todo:
+// Retrieve usersFollowed: https://noroff-api-docs.netlify.app/social-endpoints/profiles#single-entry
+// Toggle follow/unfollow
+
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
 import { apiBaseUrl, apiToken } from "../constants/variables";
 
 const usersApiUrl = apiBaseUrl + "/profiles";
@@ -9,6 +13,7 @@ function ViewProfiles() {
     const [loadingProfiles, setLoadingProfiles] = useState(false);
     const [users, setUsers] = useState([]);
     const [error, setError] = useState(null);
+    const [usersFollowed, setUsersFollowed] = useState([]);
 
     useEffect(() => {
         async function getUsers() {
@@ -27,31 +32,47 @@ function ViewProfiles() {
         getUsers();
     }, []);
 
+    useEffect(() => {
+        async function getUsersFollowed() {}
+    });
+
+    function toggleFollow(e) {
+        console.log(e);
+        console.log(e.target.dataset.username);
+    }
     return (
-        <ul>
-            {users.map((profile) => (
-                <li key={profile.name} className="user">
-                    <Row className="user__profile">
-                        {/* <div className="user__profile"> */}
-                        <Col className="user__profile-imagecontainer">{profile.avatar ? <img src={profile.avatar} className="user__profile-imagecontainer-img" /> : ""}</Col>
-                        <Col className="user__profile-content">
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Avatar</th>
+                    <th>User Info</th>
+                    <th>Stats</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((profile) => (
+                    <tr>
+                        <td className="user__profile-imagecontainer">{profile.avatar ? <img src={profile.avatar} className="user__profile-imagecontainer-img" /> : ""}</td>
+                        <td className="user__profile-content">
                             <p className="user__profile-content-name">{profile.name}</p>
                             <p className="user__profile-content-email">
                                 <a href={`mailto:${profile.email}`} className="user__profile-content-email-link">
                                     {profile.email}
                                 </a>
                             </p>
-                        </Col>
-                        <Col className="user__profile-counts">
-                            <p>Followers: {profile._count.followers}</p>
-                            <p>Following: {profile._count.following}</p>
-                        </Col>
-                        {/* <div className="user__profile-content"></div> */}
-                        {/* </div> */}
-                    </Row>
-                </li>
-            ))}
-        </ul>
+                        </td>
+                        <td className="user__profile-counts">
+                            <p className="user__profile-counts-detail">Posts: {profile._count.posts}</p>
+                            <p className="user__profile-counts-detail">Followers: {profile._count.followers}</p>
+                            <p className="user__profile-counts-detail">Following: {profile._count.following}</p>
+                            <Button data-username={profile.name} size="sm" variant="success" onClick={toggleFollow}>
+                                Follow
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     );
 }
 
