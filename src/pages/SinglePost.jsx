a893ffcf975164ba87c7261ea72b10f3aeff2feb
stian-git <82411646+stian-git@users.noreply.github.com>
2023-02-22 18:15:30 +0100

    Added errorhandling of singlepost

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ShowPost from "../components/ShowPost";
+import ShowStatusMessage from "../components/ShowStatusMessage";
 import ShowUserDetails from "../components/ShowUserDetails";
 
 import useGetSinglePost from "../hooks/useGetSinglePost";
@@ -11,20 +12,37 @@ function SinglePost() {
   const { postid } = useParams();
   const { postData, loading, error } = useGetSinglePost(postid);
   const { userData, userLoading, userError } = useGetSingleProfile(postData?.author?.name);
+  const history = useNavigate();
 
   return (
     <>
-      <ShowPost postdata={postData} comments={false} showlarge={true} />
-      {userData ? (
-        <Container className="large">
-          <h1>Published by</h1>
-          <ShowUserDetails userprofile={userData} />
-        </Container>
+      {" "}
+      {error ? (
+        <>
+          <ShowStatusMessage text={`Post ID: ${postid} could not be loaded and probably do not exist.`} display={true} />
+          <p
+            className="link"
+            onClick={() => {
+              history(-1);
+            }}>
+            Click here to go back.
+          </p>
+        </>
       ) : (
-        ""
-      )}
+        <>
+          <ShowPost postdata={postData} comments={false} showlarge={true} />
+          {userData ? (
+            <Container className="large">
+              <h1>Published by</h1>
+              <ShowUserDetails userprofile={userData} />
+            </Container>
+          ) : (
+            ""
+          )}
 
-      {userLoading ? <p>Loading</p> : ""}
+          {userLoading ? <p>Loading</p> : ""}
+        </>
+      )}
     </>
   );
 }
