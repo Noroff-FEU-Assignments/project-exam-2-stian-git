876deaf099abe536e178de5bad04131bc97631bc
stian-git <82411646+stian-git@users.noreply.github.com>
2023-02-23 16:13:03 +0100

    Removed usersfollowed from context

--- src/context/SessionContext.jsx
+++ src/context/SessionContext.jsx
@@ -7,26 +7,6 @@ const SessionContext = React.createContext([null, () => {}]);
 
 export const SessionProvider = (props) => {
   const [loggedIn, setLoggedIn] = useLocalStorage(storageKeySessionInfo, null);
-  const [setUsersFollowed] = useLocalStorage(storageKeyFollowedUsers, []);
-
-  useEffect(() => {
-    async function setFollowedUsers() {
-      const getProfileApiUrl = apiBaseUrl + "/profiles/" + loggedIn.name + "?_following=true&_followers=true";
-      try {
-        axios.defaults.headers.common = { Authorization: `Bearer ${loggedIn.accessToken}` };
-        const response = await axios(getProfileApiUrl);
-        if (response.status === 200) {
-          const data = await response.data;
-          setUsersFollowed(data.following);
-        }
-      } catch (error) {
-        //This error does not affect the user experience and will be ignored.
-      }
-    }
-    if (loggedIn) {
-      setFollowedUsers();
-    }
-  }, [loggedIn, setUsersFollowed]);
   return <SessionContext.Provider value={[loggedIn, setLoggedIn]}>{props.children}</SessionContext.Provider>;
 };
 
